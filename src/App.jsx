cfedf12c80fe9513226677868f3987a6bd4fa3f1
Harn1sh <41642384+Harn1sH@users.noreply.github.com>
2023-05-28 17:38:34 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
+import Detailed from "./components/Detailed";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../src/index.css";
 import Comp1 from "./components/comp1";
 function App() {
   return (
-    <>
-      <h1 className="text-lg tracking-widest text-blue-200">
-        <Comp1 />
-      </h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Comp1 />}></Route>
+        <Route path="/:id" element={<Detailed />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
